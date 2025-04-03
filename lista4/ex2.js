db6d1cd3aa80329193a23482f4764cd728b3cf66
Pedro Nishimura <129108608+PhNishimura@users.noreply.github.com>
2025-04-03 14:57:15 -0300

    Delete lista4 directory

--- lista4/ex2.js
+++
@@ -1,9 +0,0 @@
-function doAction(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Olá, mundo");
-        }, 2000); 
-    })
-}
-
-    doAction().then(mensagem => console.log(mensagem));
